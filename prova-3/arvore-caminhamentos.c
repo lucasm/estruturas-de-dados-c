484260f70326eb0f6d891c32c3f0ca73f85d9e91
Lucas Menezes <lucasm@lucasm.dev>
2023-12-14 19:49:23 -0300

    caminhamentos

---
+++ prova-3/arvore-caminhamentos.c
@@ -0,0 +1,100 @@
+/*
+
+Considere uma árvore binária de busca com os seguintes valores: 50, 30, 70, 20, 40, 60, 80.
+
+Implemente um código em C para realizar os três tipos de percurso na árvore: pré-ordem, em ordem e pós-ordem.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+  int data;
+  struct Node *left;
+  struct Node *right;
+};
+
+struct Node *createNode(int value)
+{
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = value;
+  newNode->left = NULL;
+  newNode->right = NULL;
+  return newNode;
+}
+
+struct Node *insert(struct Node *node, int value)
+{
+  if (node == NULL)
+  {
+    return createNode(value);
+  }
+
+  if (value < node->data)
+  {
+    node->left = insert(node->left, value);
+  }
+  else if (value > node->data)
+  {
+    node->right = insert(node->right, value);
+  }
+
+  return node;
+}
+
+void preOrderTraversal(struct Node *node)
+{
+  if (node != NULL)
+  {
+    printf("%d ", node->data);
+    preOrderTraversal(node->left);
+    preOrderTraversal(node->right);
+  }
+}
+
+void inOrderTraversal(struct Node *node)
+{
+  if (node != NULL)
+  {
+    inOrderTraversal(node->left);
+    printf("%d ", node->data);
+    inOrderTraversal(node->right);
+  }
+}
+
+void postOrderTraversal(struct Node *node)
+{
+  if (node != NULL)
+  {
+    postOrderTraversal(node->left);
+    postOrderTraversal(node->right);
+    printf("%d ", node->data);
+  }
+}
+
+int main()
+{
+  struct Node *root = NULL;
+  int elements[] = {50, 30, 70, 20, 40, 60, 80};
+
+  for (int i = 0; i < sizeof(elements) / sizeof(elements[0]); i++)
+  {
+    root = insert(root, elements[i]);
+  }
+
+  printf("Percurso Pré-ordem: ");
+  preOrderTraversal(root);
+  printf("\n");
+
+  printf("Percurso Em ordem: ");
+  inOrderTraversal(root);
+  printf("\n");
+
+  printf("Percurso Pós-ordem: ");
+  postOrderTraversal(root);
+  printf("\n");
+
+  return 0;
+}
